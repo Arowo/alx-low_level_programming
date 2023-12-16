1b09d876c85b4f25774ff26905f3ef9817752e13
Arowo <Arowobusoyesaanu088@gmail.com>
2023-12-15 17:58:13 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This function is the entry point of the program.
+ *              It prints a specific message and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    puts("Programming is like building a multilingual puzzle");
     return (0);
 }
