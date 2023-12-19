16f3ce6431d00b569d439098cca8d5dc3729a504
Arowo <Arowobusoyesaanu088@gmail.com>
2023-12-19 01:30:39 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,17 @@ int main(void)
     printf("%d ", n);
 
     if (n > 0)
+    {
         printf("is positive\n");
+    }
     else if (n == 0)
+    {
         printf("is zero\n");
+    }
     else
+    {
         printf("is negative\n");
+    }
 
-    return 0;
+    return (0);
 }
