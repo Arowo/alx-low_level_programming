edd68c9d6ebe697938e9550a532856efdd80822c
Arowo <Arowobusoyesaanu088@gmail.com>
2023-12-20 12:02:10 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,20 +10,20 @@
  */
 int main(void)
 {
-	int number;
+    int number;
 
-	for (number = 0; number <= 9; number++)
-	{
-		putchar(number + '0');
+    for (number = 0; number <= 9; number++)
+    {
+        putchar(number + '0');
 
-		if (number != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+        if (number != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
