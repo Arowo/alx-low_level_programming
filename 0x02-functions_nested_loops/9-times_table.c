099182a6718305fc05089469ae0f2186b218f61a
Arowo <Arowobusoyesaanu088@gmail.com>
2023-12-25 01:34:17 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0
- *
- * Return: No return value (void function)
+ * times_table - Prints the 9 times table.
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, product;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-
-            /* Print result with proper formatting */
-            if (result < 10 && j > 0)
-                _putchar(' ');
-            else if (j > 0)
-                _putchar((result / 10) + '0');
-
-            _putchar((result % 10) + '0');
-
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i <= 9; ++i)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			product = i * j;
+			if (j == 0)
+				_putchar('0');
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
