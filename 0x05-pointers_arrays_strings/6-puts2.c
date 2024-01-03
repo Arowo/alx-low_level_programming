ab0a59d8b75e1e46ff6667c325257a324a918889
Arowo <Arowobusoyesaanu088@gmail.com>
2024-01-03 05:43:38 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first character.
+ * @str: Pointer to the string.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	/* Iterate through the string */
+	while (str[i] != '\0')
+	{
+		/* Print the character if the index is even */
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
