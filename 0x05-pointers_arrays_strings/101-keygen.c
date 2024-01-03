3dad9edea37850e37bf133f167885daa81524321
Arowo <Arowobusoyesaanu088@gmail.com>
2024-01-03 06:23:05 -0800

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-    int sum = 2772;
-    char password[9];
+    int sum = 0;
+    char password[7];
     int i, r;
 
     srand(time(0));
 
-    for (i = 0; i < 8; i++)
+    for (i = 0; i < 6; i++)
     {
-        r = rand() % 94 + 33;
-        password[i] = r;
-        sum -= r;
+        r = rand() % 10;
+        password[i] = '0' + r;
+        sum += r;
     }
 
-    password[i] = sum;
+    password[i] = '0' + (10 - sum % 10);
 
     printf("%s", password);
 
