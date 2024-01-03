e0b169328662b0bd0bbb29c3b5146d75bfd2e2ed
Arowo <Arowobusoyesaanu088@gmail.com>
2024-01-03 07:18:51 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,20 +10,20 @@
 int main(void)
 {
     int sum = 0;
-    char password[15];
+    char password[12]; // Adjusted password length to 11 characters + null terminator
     int i, r;
 
     srand(time(0));
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 11; i++) // Loop to fill the entire password array except the last character
     {
-        r = rand() % 94 + 33;
+        r = rand() % 94 + 33; // Generate characters in the range [33, 126]
         password[i] = r;
         sum += r;
     }
 
-    password[i++] = (sum % 94) + 33;
-    password[i] = '\0';
+    password[i++] = (2772 - sum) % 94 + 33; // Adjust the last character based on the desired sum
+    password[i] = '\0'; // Terminate the string
 
     printf("%s\n", password);
 
