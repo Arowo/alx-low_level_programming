5fe3d669763fd77a60218ea966496ff45ae12d31
Arowo <Arowobusoyesaanu088@gmail.com>
2024-01-05 05:16:28 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,14 @@
 #include "main.h"
-#include <stddef.h>
+#include <string.h>
 
-char *_strncat(char *dest, const char *src, size_t n)
+/**
+ * _strncat - join n item in src
+ * @dest: dest
+ * @src: src
+ * @n: n
+ * Return: return dest.
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-    char *ptr = dest;
-
-    /*Move the pointer to the end of the destination string*/
-    while (*ptr != '\0')
-        ptr++;
-
-    /* Copy at most n bytes from src to the end of dest*/
-    while (*src != '\0' && n > 0)
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-        n--;
-    }
-
-    /* Add the terminating null byte*/
-    *ptr = '\0';
-
-    return dest;
+	return (strncat(dest, src, n));
 }
