9c15ea664608062e60034b5a367e85a76ca5408d
Arowo <Arowobusoyesaanu088@gmail.com>
2024-01-05 05:32:38 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,9 +3,7 @@
 
 char *_strcat(char *dest, char *src);
 
-#include <stddef.h>
-
-char *_strncat(char *dest, const char *src, size_t n);
+char *_strncat(char *, char *, int);
 
 char *_strncpy(char *dest, char *src, int n);
 
