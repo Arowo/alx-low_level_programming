ac35b453491c155996449894e77abb59f8075bcf
Arowo <Arowobusoyesaanu088@gmail.com>
2024-01-05 05:19:03 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,25 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strncat - join n item in src
- * @dest: dest
- * @src: src
- * @n: n
- * Return: return dest.
+ * _strncat - Concatenates two strings
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ * @n: Maximum number of bytes from src to be concatenated
+ *
+ * Return: Pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	return (strncat(dest, src, n));
+    int dest_len = 0;
+    int i;
+
+    /* Find the length of the destination string */
+    while (dest[dest_len] != '\0')
+        dest_len++;
+
+    /* Concatenate the characters from src to dest */
+    for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[dest_len++] = src[i];
+
+    return dest;
 }
