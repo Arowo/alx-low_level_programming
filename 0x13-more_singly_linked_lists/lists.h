161370a6e5fbf8bddb89afd7547b7280e448eb6b
Arowo <Arowobusoyesaanu088@gmail.com>
2024-02-06 04:50:01 -0800

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,16 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/* Definition of the listint_t struct */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototype for print_listint */
+size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */
