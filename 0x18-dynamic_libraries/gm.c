9de0e8c38652cce580656c0b21b0fcce17eaca2f
Arowo <Arowobusoyesaanu088@gmail.com>
2024-03-26 06:16:27 -0700

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/gm.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 7) {
+        fprintf(stderr, "Usage: %s n1 n2 n3 n4 n5 bonus\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    printf("--> Please make me win!\n");
+
+    return EXIT_SUCCESS;
+}
